4275777e1bbebe4df8b663fa084cd9052d408877
Gustavo Lopez <gustavolopezmoreno@hotmail.com>
2023-10-15 20:44:12 -0700

    Documentacion seccion de layout

--- src/pages/PDF.js
+++ src/pages/PDF.js
@@ -1,3 +1,15 @@
+// Documentacion:
+// Para correr esta parte del programa sin errores es necesario instalar 
+// un par de cosas en la computadora que se va a correr el programa
+// a continuacion se presentan los comandos para los paquetes necesarios:
+
+// npm install -g node
+// npm install react-scripts 
+// npm install pdfjs-dist
+// npm install @react-pdf-viewer/core
+// npm i @react-pdf-viewer/default-layout
+// npm install -g create-react-app
+
 import React, {useState} from "react"
 
 import { Viewer } from "@react-pdf-viewer/core";
@@ -64,7 +76,11 @@ function PDFViewer() {
             <br></br>
             <h4>View PDF</h4>
             <div className='pdf-container'>
-                {viewPdf&&<><Worker workerUrl="https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js">
+                {/* En esta seccion del codigo ocurria un error al correrlo debido a que la version de
+                pdfjs-dist no era la misma que el de la api entonces se tiene que verificar que sean
+                la misma version ambos para que corra correctamente, aqui ↓ se le puede modificar la 
+                version al pdfjs-dist para que sea la misma que la de la api que el mensaje de error arroje. */}
+                {viewPdf&&<><Worker workerUrl="https://unpkg.com/pdfjs-dist/build/pdf.worker.min.js">
                     <Viewer fileUrl={viewPdf}
                         plugins={[defaultLayoutPluginInstance]}/>
                     </Worker></>}
