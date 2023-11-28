a171f5cc7f89e80888f91195a8ab9bc0ff08e58c
David Roldan <82829595+Magnus1515@users.noreply.github.com>
2023-11-28 03:40:23 -0800

    fixing navbar and other stuff

--- src/NavBarDropdown.js
+++ src/NavBarDropdown.js
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import EXPOIng from './pages/EXPO-Ingenieria';
 import Laboratorio from './pages/Laboratorio';
@@ -11,62 +11,65 @@ import Layout from './pages/Layout.js';
 import Voting from './pages/Vote.js';
 import Login from './pages/Login';
 import TeamViewer from './pages/TeamViewer';
-// import Post from './pages/Posting';
+import Voting_kp from './pages/Vote_kp';
+import Results from './pages/Resultados';
 
 
 function PrincipalNavBarDropdown() {
   return (
     <Router>
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-      <Container>
-         <Navbar.Brand href="/">
+      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="/">
             <img className="d-inline-block align-top"
-              src = "./cetys-logo.png" alt = "logo"
+              src="./cetys-logo.png" alt="logo"
               width="70" height="60"
             />
           </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to={"/conocenos"}>CONÓCENOS</Nav.Link>
-            <NavDropdown title="EVENTOS" id="collapsible-nav-dropdown">
-              {/* <NavDropdown.Item as={Link} to={"/eventos/expo-ingenierias"}>EXPO Ingenierías</NavDropdown.Item> */}
-                <NavDropdown title="EXPO Ingenierias "id="collapsible-nav-dropdown">
-                <NavDropdown.Item as={Link} to={"/eventos/expo-ingenierias"}>Inicio</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to={"/eventos/expo-ingenierias/layout"}>Layout</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to={"/eventos/expo-ingenierias/votación"}>Votación</NavDropdown.Item>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to={"/conocenos"}>CONÓCENOS</Nav.Link>
+              <NavDropdown title="EVENTOS" id="collapsible-nav-dropdown">
+                {/* <NavDropdown.Item as={Link} to={"/eventos/expo-ingenierias"}>EXPO Ingenierías</NavDropdown.Item> */}
+                <NavDropdown title="EXPO Ingenierias " id="collapsible-nav-dropdown">
+                  <NavDropdown.Item as={Link} to={"/eventos/expo-ingenierias"}>Inicio</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to={"/eventos/expo-ingenierias/layout"}>Mapa</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to={"/eventos/expo-ingenierias/votacion-resultados"}>Resultados Votación</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to={"/teamviewer"}>Proyectos</NavDropdown.Item>
                 </NavDropdown>
-              {/* <NavDropdown.Item href="#eventos/3.2"> Evento 2</NavDropdown.Item>
+                {/* <NavDropdown.Item href="#eventos/3.2"> Evento 2</NavDropdown.Item>
               <NavDropdown.Item href="#eventos/3.3"> Evento 3</NavDropdown.Item> */}
-            </NavDropdown>
-            <Nav.Link as={Link} to={"/laboratorio"}>LABORATORIO</Nav.Link>
-            <Nav.Link as={Link} to={"/teamviewer"}>PROYECTOS</Nav.Link>
-            {/* <Nav.Link as={Link} to={"/post"}>POST</Nav.Link> */}
-          </Nav>
-        </Navbar.Collapse>
-        <Nav.Link as={Link} to="/user-login">
+              </NavDropdown>
+              <Nav.Link as={Link} to={"/laboratorio"}>LABORATORIO</Nav.Link>
+              
+              {/* <Nav.Link as={Link} to={"/post"}>POST</Nav.Link> */}
+            </Nav>
+          </Navbar.Collapse>
+          <Nav.Link as={Link} to="/user-login">
             <img className="d-inline-block align-top"
               src="./user-icon2.png"
               alt="User Profile"
-              width="55"
-              height="55"
+              width="35"
+              height="35"
             />
           </Nav.Link>
-      </Container>
-    </Navbar>
-    <div>
-      <Routes>
-        <Route path="/conocenos" element={<Conocenos />}/>
-        <Route path="/eventos/expo-ingenierias" element={<EXPOIng />}/>
-        <Route path="/laboratorio" element={<Laboratorio />}/>
-        <Route path="/eventos/expo-ingenierias/layout" element={<Layout />}/>
-        <Route path="/eventos/expo-ingenierias/votación" element={<Voting />}/>
-        <Route path="/user-login" element={<Login />} />
-        <Route path="/teamviewer" element={<TeamViewer />}/>
-        {/* <Route path="/post" element={<Post />}/> */}
-      </Routes>
-    </div>
-    </Router>    
+        </Container>
+      </Navbar>
+      <div>
+        <Routes>
+          <Route path="/conocenos" element={<Conocenos />} />
+          <Route path="/eventos/expo-ingenierias" element={<EXPOIng />} />
+          <Route path="/laboratorio" element={<Laboratorio />} />
+          <Route path="/eventos/expo-ingenierias/layout" element={<Layout />} />
+          <Route path="/eventos/expo-ingenierias/votacion-resultados" element={<Results />} />
+          <Route path="/user-login" element={<Login />} />
+          <Route path="/teamviewer" element={<TeamViewer />} />
+          <Route path="/eventos/expo-ingenierias/votacion/votacion-kn" element={<Voting_kp />} />
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
